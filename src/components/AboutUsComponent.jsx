b4bf46225954dae3ba68501d67058b78ad360dcc
Valentin <valentin.etcheberry@davinci.edu.ar>
2025-02-25 17:46:06 -0300

    d

--- src/components/AboutUsComponent.jsx
+++ src/components/AboutUsComponent.jsx
@@ -3,48 +3,49 @@ import ImageCarousel from './ImageCarousel';
 import { AboutUsImages } from '../constants';
 
 const AboutUsComponent = () => {
-  return (
-    <section className="grid grid-cols-12 mx-5 pb-20 pt-12 lg:pt-24  items-center">
+    return (
+      <section className="grid grid-cols-12 pb-20 pt-12 lg:pt-24 lg:gap-5 items-center mx-5 md:mx-0">
         <h2 className="sr-only">Sobre nosotros</h2>
-
+  
         {/* Contenedor del carrusel */}
         <div
-            className="col-span-12 lg:col-span-5 lg:col-start-2 flex justify-center"
-            data-aos="fade-up"
-            data-aos-duration="500"
-            data-aos-easing="ease-in-out"
-            data-aos-delay="100"
+          className="col-span-12 lg:col-span-5 lg:col-start-2 flex justify-center lg:ps-4"
+          data-aos="fade-up"
+          data-aos-duration="500"
+          data-aos-easing="ease-in-out"
+          data-aos-delay="100"
         >
-            <ImageCarousel images={AboutUsImages} alt="Testimonial" />
+          <ImageCarousel images={AboutUsImages} alt="Testimonial" />
         </div>
-
+  
         {/* Contenedor del texto */}
         <div
-            className="col-span-12 lg:col-span-5 px-2"
-            data-aos="fade-up"
-            data-aos-duration="500"
-            data-aos-easing="ease-in-out"
-            data-aos-delay="100"
+          className="col-span-12 lg:col-span-5 lg:pe-4"
+          data-aos="fade-up"
+          data-aos-duration="500"
+          data-aos-easing="ease-in-out"
+          data-aos-delay="100"
         >
-            <h3 className="text-3xl lg:text-5xl font-medium mb-3">
-            Hemos trabajado en  canchas de Argentina, Chile, Uruguay y Brasil.
-            </h3>
-            <p className="px-1 lg:text-lg">
+          <h3 className="text-3xl lg:text-5xl font-medium mb-3">
+            Hemos trabajado en canchas de Argentina, Chile, Uruguay y Brasil.
+          </h3>
+          <p className="lg:text-lg">
             Muchos de estos proyectos fueron con diseño propio y otros en
             colaboración o en la ejecución de proyectos de otros diseñadores como Ken
             Dye, Bob Cupp, Gary Player Design y American Golf Design, entre otros.
-            </p>
-            <div className="mt-4 lg:mt-6 flex justify-start gap-2">
+          </p>
+          <div className="mt-4 lg:mt-6 flex justify-start gap-2">
             <a
-                href="/sobre-nosotros"
-                className="bg-green-950 text-white font-bold py-3 px-6 rounded-lg hover:bg-green-900 transition duration-500 flex items-center gap-2"
+              href="/sobre-nosotros"
+              className="bg-green-950 text-white font-bold py-3 px-6 rounded-lg hover:bg-green-900 transition duration-500 flex items-center gap-2"
             >
-                Sobre nosotros
+              Sobre nosotros
             </a>
-            </div>
+          </div>
         </div>
-        </section>
-  );
-};
+      </section>
+    );
+  };
+  
 
 export default AboutUsComponent
--- src/components/TestimonialCarousel.jsx
+++ src/components/TestimonialCarousel.jsx
@@ -4,32 +4,33 @@ import { testimonialImages } from '../constants';
 
 const TestimonialCarousel = () => {
   return (
-    <section className="py-16 mb-20 px-5 lg:px-20"  data-aos="fade-up" data-aos-duration="800" data-aos-easing="ease-in-out">
-      <div className="max-w-screen-xl mx-auto grid grid-cols-12 gap-8">
-        {/* Texto */}
-        <div className="col-span-12 lg:col-span-6 flex flex-col justify-center space-y-6 order-2 lg:order-1">
-          <h2 className="text-3xl lg:text-5xl font-medium">
-            Representantes exclusivos para Latinoamérica
-          </h2>
-          <p className="text-lg text-gray-700">
-            Nos enorgullece ser los representantes oficiales de{" "}
-            <span className="font-semibold">Gary Player Design</span> en
-            Latinoamérica. Con una trayectoria de excelencia en diseño de
-            campos de golf, llevamos a la región la visión y experiencia de una
-            leyenda del golf.
-          </p>
-          <blockquote className="border-l-4 border-green-950 pl-4 italic text-gray-600">
-            "Diseñar campos de golf no es solo un arte, sino un legado para las
-            futuras generaciones." - Gary Player
-          </blockquote>
-        </div>
-        <div className="col-span-12 lg:col-span-6 flex justify-center order-1 lg:order-2">
-          <ImageCarousel images={testimonialImages} alt="Testimonial" />
-        </div>
+    <section
+      className="grid grid-cols-12 pb-20 pt-12 lg:pt-24 lg:gap-5 items-center mx-5 md:mx-0"
+      data-aos="fade-up"
+      data-aos-duration="800"
+      data-aos-easing="ease-in-out"
+    >
+      {/* Contenedor del texto */}
+      <div className="col-span-12 lg:col-span-5 lg:col-start-2 lg:ps-4 order-2 lg:order-1">
+        <h2 className="text-3xl lg:text-5xl font-medium">
+          Representantes exclusivos para Latinoamérica
+        </h2>
+        <p className="text-lg text-gray-700">
+          Nos enorgullece ser los representantes oficiales de{" "}
+          <span className="font-semibold">Gary Player Design</span> en Latinoamérica. Con una trayectoria de excelencia en diseño de campos de golf, llevamos a la región la visión y experiencia de una leyenda del golf.
+        </p>
+        <blockquote className="border-l-4 border-green-950 pl-4 italic text-gray-600">
+          "Diseñar campos de golf no es solo un arte, sino un legado para las futuras generaciones." - Gary Player
+        </blockquote>
       </div>
-      
+
+      {/* Contenedor del carrusel */}
+      <div className="col-span-12 lg:col-span-5 lg:col-start-7 flex justify-center lg:pe-4 order-1 lg:order-2">
+        <ImageCarousel images={testimonialImages} alt="Testimonial" />
+      </div>
     </section>
   );
 };
+
 
 export default TestimonialCarousel
