9cf5a0363fd14ce54893b8c2285d740b1cdc8eaf
Valentin <valentin.etcheberry@davinci.edu.ar>
2025-02-25 17:27:15 -0300

    sad

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -8,7 +8,14 @@ const ImageCarousel = ({ images, alt }) => {
     setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  return (
+  // Función para retroceder al slide anterior
+  const prevSlide = () => {
+    setActiveIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+   return (
     <div className="relative w-full">
       {/* Contenedor que usa aspect ratio para mantener proporciones */}
       <div className="relative overflow-hidden rounded-lg w-full aspect-[16/9]">
@@ -26,10 +33,32 @@ const ImageCarousel = ({ images, alt }) => {
             />
           </div>
         ))}
+
+        {/* Botones de navegación */}
+        <div className="absolute inset-0 flex items-center justify-between z-30">
+          <button
+            onClick={prevSlide}
+            className="text-white rounded-full p-2 hover:bg-opacity-75 focus:outline-none"
+          >
+            {/* Icono de flecha izquierda */}
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+            </svg>
+          </button>
+          <button
+            onClick={nextSlide}
+            className="text-white rounded-full p-2 hover:bg-opacity-75 focus:outline-none"
+          >
+            {/* Icono de flecha derecha */}
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+            </svg>
+          </button>
+        </div>
       </div>
 
       {/* Indicadores de paginación achicados */}
-      <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-1 z-20">
+     {/*  <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-1 z-20">
         {images.map((_, idx) => (
           <span
             key={idx}
@@ -39,7 +68,7 @@ const ImageCarousel = ({ images, alt }) => {
             }`}
           />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
